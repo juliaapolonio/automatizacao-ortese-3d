2e8630a8c714c9d74eacc8b1454a1c9e64c7f8d2
Julia Apolonio <apoloniojulia@gmail.com>
2020-06-10 01:04:02 -0300

    feat (scripts): add slicer in python

---
+++ scripts/slicer.py
@@ -0,0 +1,7 @@
+import subprocess
+
+def runSlicer():
+    subprocess.call("../Slic3r/perl-local -I../Slic3r/local-lib/lib/perl5 ../Slic3r/slic3r.pl" + 
+    " ../data/outputCAD.stl --load ../data/my_config.ini --output ../data/saida.gcode", shell = True)
+
+runSlicer()
