5bf4bd80c26548731b12be105ae49bc154128444
Julia Apolonio <apoloniojulia@gmail.com>
2020-06-10 02:07:30 -0300

    build(*): Delete Slic3r folder

--- Slic3r/gera_gcode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#rotina para impressão
-
-BIN=$(readlink "$0")
-DIR=$(dirname "$BIN")
-export LD_LIBRARY_PATH="$DIR/bin"
-exec "$DIR/perl-local" -I"$DIR/local-lib/lib/perl5" "$DIR/slic3r.pl" "../data/outputCAD.stl" "--load" "my_config.ini" "--print-center" "100,100" "--output" "../data/saida.gcode" $@
