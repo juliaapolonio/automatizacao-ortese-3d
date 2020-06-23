477d8fec2647990513e5ebc30989a811d73a3d4e
Julia Apolonio <apoloniojulia@gmail.com>
2020-06-23 10:43:04 -0300

    refactor(/scripts): delete useless line
    Co-authored-by: Diego Coelho <diego.coelho@mendelics.com.br>

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 conda create -n ort3d -y python=3.7
-source activate ort3d
 conda install -n ort3d -c conda-forge -y freecad
 ~/miniconda3/envs/ort3d/bin/pip install -r requirements.txt
 wget https://dl.slic3r.org/linux/slic3r-1.3.0-linux-x64.tar.bz2
