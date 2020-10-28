afa1eeae267d6c40cd0203d1e8640e45083b9d1e
Julia Apolonio <apoloniojulia@gmail.com>
2020-10-28 16:55:50 -0300

    fix(/): models downloaded with install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ conda install -n ort3d -c conda-forge -y freecad
 wget https://dl.slic3r.org/linux/slic3r-1.3.0-linux-x64.tar.bz2
 bzip2 -dc slic3r-1.3.0-linux-x64.tar.bz2 | tar xvf -
 rm *.bz2
+cd "$(data/models "$0")"
+wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/hand/pose_iter_102000.caffemodel
