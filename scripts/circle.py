ec5e998f6ceb9bf19c1e6fddb2d4d0f1a9de23a6
Julia Apolonio <apoloniojulia@gmail.com>
2020-11-03 22:05:39 -0300

    fix(*): Coin detection enhancement

--- scripts/circle.py
+++ scripts/circle.py
@@ -15,15 +15,28 @@ def crl(path):
 	output = image.copy()
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Detect circles in the image
-	circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 20, param1=150, param2=30, minRadius=0, maxRadius=0)
-	
-# Ensure at least some circles were found
+	gaussian = cv2.GaussianBlur(gray, (11, 11), 0) #5 5
+
+	canny = cv2.Canny(gaussian, 40, 100) #100 200
+
+	rows = canny.shape[0]
+	circles = cv2.HoughCircles(canny, cv2.HOUGH_GRADIENT, 1, rows/8, param1=100, param2=20, minRadius=20, maxRadius=100)
+
 	if circles is not None:
-		
-# Convert the (x, y) coordinates and radius of the circles to integers
-		circles = np.round(circles[0, :]).astype("int")
+		circles_np = np.uint16(np.around(circles))
+		for i in circles_np[0, :]:
+			center = (i[0], i[1])
+			cv2.circle(output, center, 1, (0, 100, 100), 3)
+			radius = i[2]
+			cv2.circle(output, center, radius, (255, 0, 255), 3)
+
+	output = cv2.resize(output, (500, 500))
+#	cv2.imshow('image', output)
+#	cv2.waitKey(0)
+#	cv2.destroyAllWindows()
 
 # Return values
-		for (x,y,r) in circles:
-			return (x,y,r)
+	for (x,y,r) in circles[0]:
+		return (x,y,r)
+
+#(x,y,r) = crl('../data/images/sevs.jpeg')
